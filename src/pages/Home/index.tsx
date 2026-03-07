2c6183262cf05b99b6fef375af856a6f9787ed68
yumiaomiao <yumiaomiao@hello.com>
2026-03-07 10:59:37 +0800

    fix: test cloudflare auto deploy

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -19,7 +19,7 @@ export function HomePage() {
           <div className="flex items-center gap-4">
             <span className="font-mono text-5xl font-700 text-coral">&gt;</span>
             <h1 className="font-heading text-5xl font-700 text-text-primary m-0">
-              你好，我是 YuQiu
+              你好，我是 YuQiu...
             </h1>
           </div>
         }
